5cc0e38d0b4806119bb28c1493f8eae8963ed037
Samer Kanjo <samer@kanjo.net>
2023-03-05 21:34:03 -0600

    ipv4 scanner working

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject dipfins "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/data.json "2.4.0"]
+                 [com.amazonaws/aws-lambda-java-core "1.2.2"]]
+  :aot :all)
---
+++ src/dipfins/ip_address.clj
@@ -0,0 +1,23 @@
+(ns dipfins.ip_address
+  (:require [clojure.data.json :as json]))
+
+(def ipv4-pattern (re-pattern "^(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])$"))
+
+(defn is-ipv4? [s]
+  (if (string? s)
+    (some? (re-matches ipv4-pattern s))
+    false))
+
+(defn scan-for-ipv4 [m]
+  (reduce-kv
+    (fn [acc _ v]
+      (cond
+        (nil? v) acc
+        (string? v) (if (is-ipv4? v) (conj acc v) acc)
+        (or (map? v) (vector? v)) (clojure.set/union acc (scan-for-ipv4 v))
+        :else acc))
+    #{}
+    m))
+
+(defn read-file [f]
+  (json/read-str (slurp "resources/3.in.json")))
