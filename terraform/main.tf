d1cdf01cbef4f0909dbce1941eb5fe8bc8c9c9a0
Samer Kanjo <samer@kanjo.net>
2023-03-06 16:09:27 -0600

    add s3 bucket and lambda notification

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,6 +16,7 @@ locals {
   lambda_name       = "dipfins-${var.stage}"
   lambda_build_path = "../target/dipfins-0.1.0-SNAPSHOT-standalone.jar"
   lambda_handler    = "dipfins.core.handler"
+  s3_bucket_name    = "dipfins-store-${var.stage}"
 }
 
 module "lambda" {
@@ -29,4 +30,30 @@ module "lambda" {
 
   create_package         = false
   local_existing_package = local.lambda_build_path
+}
+
+module "s3_bucket" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-s3-bucket?ref=v3.7.0"
+
+  bucket = local.s3_bucket_name
+  acl    = "private"
+
+  versioning = {
+    enabled = false
+  }
+}
+
+module "s3_bucket_notification" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-s3-bucket.git//modules/notification?ref=v3.7.0"
+
+  bucket = module.s3_bucket.s3_bucket_id
+
+  lambda_notifications = {
+    lambda = {
+      function_arn  = module.lambda.lambda_function_arn
+      function_name = module.lambda.lambda_function_name
+      events        = ["s3:ObjectCreated:*"]
+      filter_suffix = ".json"
+    }
+  }
 }
