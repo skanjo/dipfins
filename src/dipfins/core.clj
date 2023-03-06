eb3f801cabf831f197d9fa90f2d716ac62837e65
Samer Kanjo <samer@kanjo.net>
2023-03-06 11:16:11 -0600

    working lambda using requeststreamhandler

--- src/dipfins/core.clj
+++ src/dipfins/core.clj
@@ -1,27 +1,36 @@
 (ns dipfins.core
-  (:gen-class
-    :methods [^:static [handler [java.util.Map] String]]))
+  (:require [clojure.java.io :as io])
+  (:import (com.amazonaws.services.lambda.runtime Context)))
 
+(gen-class
+  :name "dipfins.core.handler"
+  :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler])
 
-(defprotocol ConvertibleToClojure
-  (->cljmap [o]))
+;(defprotocol ConvertibleToClojure
+;  (->cljmap [o]))
+;
+;(extend-protocol ConvertibleToClojure
+;  java.util.Map
+;  (->cljmap [o] (let [entries (.entrySet o)]
+;                  (reduce (fn [m [^String k v]]
+;                            (assoc m (keyword k) (->cljmap v)))
+;                          {} entries)))
+;
+;  java.util.List
+;  (->cljmap [o] (vec (map ->cljmap o)))
+;
+;  java.lang.Object
+;  (->cljmap [o] o)
+;
+;  nil
+;  (->cljmap [_] nil))
+;
+;(defn -handler [s]
+;  (println (->cljmap s))
+;  (println "Hello World!"))
 
-(extend-protocol ConvertibleToClojure
-  java.util.Map
-  (->cljmap [o] (let [entries (.entrySet o)]
-                  (reduce (fn [m [^String k v]]
-                            (assoc m (keyword k) (->cljmap v)))
-                          {} entries)))
-
-  java.util.List
-  (->cljmap [o] (vec (map ->cljmap o)))
-
-  java.lang.Object
-  (->cljmap [o] o)
-
-  nil
-  (->cljmap [_] nil))
-
-(defn -handler [s]
-  (println (->cljmap s))
-  (println "Hello World!"))
+(defn -handleRequest
+  [this input-stream output-stream context]
+  (let [handle (io/writer output-stream)]
+    (.write handle (str "hello" "world"))
+    (.flush handle)))
