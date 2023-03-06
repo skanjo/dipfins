0da05efed0a6a45427cdeea04a57cc5102265c5c
Samer Kanjo <samer@kanjo.net>
2023-03-06 10:52:33 -0600

    Updated project file

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject dipfins "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Extract IPv4 addresses from S3 file and write result back to S3"
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/data.json "2.4.0"]
                  [com.amazonaws/aws-lambda-java-core "1.2.2"]]
