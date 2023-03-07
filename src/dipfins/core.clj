4c6b2db463bbd1f3a9c92aa84ffecf56dccae4d1
Samer Kanjo <samer@kanjo.net>
2023-03-06 18:46:36 -0600

    removed unused code

--- src/dipfins/core.clj
+++ src/dipfins/core.clj
@@ -1,39 +1,15 @@
 (ns dipfins.core
   (:require [clojure.data.json :as json]
-            [clojure.java.io :as io])
-  (:import (com.amazonaws.services.lambda.runtime Context)))
+            [clojure.java.io :as io]))
 
 (gen-class
   :name "dipfins.core.handler"
   :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler])
 
-;(defprotocol ConvertibleToClojure
-;  (->cljmap [o]))
-;
-;(extend-protocol ConvertibleToClojure
-;  java.util.Map
-;  (->cljmap [o] (let [entries (.entrySet o)]
-;                  (reduce (fn [m [^String k v]]
-;                            (assoc m (keyword k) (->cljmap v)))
-;                          {} entries)))
-;
-;  java.util.List
-;  (->cljmap [o] (vec (map ->cljmap o)))
-;
-;  java.lang.Object
-;  (->cljmap [o] o)
-;
-;  nil
-;  (->cljmap [_] nil))
-;
-;(defn -handler [s]
-;  (println (->cljmap s))
-;  (println "Hello World!"))
-
 (defn -handleRequest
-  [this input-stream output-stream context]
-  (prn (json/read-str (slurp input-stream)))
+  [_this in out context]
+  (prn (json/read-str (slurp in)))
   (prn (.getAwsRequestId context))
-  (let [handle (io/writer output-stream)]
+  (let [handle (io/writer out)]
     (.write handle (str "hello" "world"))
     (.flush handle)))
